a3bb924cf695fbe6ed9db674d1aa73f00bc1a778
Luis0 <66780671+ErLuize@users.noreply.github.com>
2025-12-16 17:01:11 +0100

    Delete inicio-eventos-eventos_admin directory

--- inicio-eventos-eventos_admin/js/Donacion.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll('.btn-money');
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        buttons.forEach(b => b.classList.remove('active'));
-        btn.classList.add('active');
-    });
-});
